257e82b6af3cec560098aca718c451b0202c8501
kkonteh97 <55326260+kkonteh97@users.noreply.github.com>
2023-11-06 15:41:25 -0800

    Initial Commit

---
+++ MediumFunctionCalls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediumFunctionCalls
+//
+//  Created by kemo konteh on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediumFunctionCalls/MediumFunctionCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumFunctionCallsApp.swift
+//  MediumFunctionCalls
+//
+//  Created by kemo konteh on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MediumFunctionCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
